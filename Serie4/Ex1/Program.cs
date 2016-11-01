f3b2db91b1a97908a9e8d9c07b4dd414c3ca06ad
lancelot <lancelotlancelot94@gmail.com>
2016-11-01 20:04:35 +0100

    redefine ToString to get HH:MM format for Time objects

--- Serie4/Ex1/Program.cs
+++ Serie4/Ex1/Program.cs
@@ -12,24 +12,29 @@ namespace Ex1
         {
             Time t = new Time(System.DateTime.Now.Hour,System.DateTime.Now.Minute);
             Console.WriteLine("Temps écoulé ce jour: "+t+" minutes");
-            Console.WriteLine("Heures écoulées ce jour:" + t + ")");
+            Console.WriteLine("Heures écoulées ce jour:(H)" + t.Hours + "==(Min)"+t.Minutes);
 
+            Console.WriteLine("test" + (float)t.Minutes/60);
+            
             Console.ReadKey();
         }
     }
     public struct Time
     {
         private readonly int minutes;
-        public readonly int Hours { get { return hours; }  }
+        private readonly int hours;
+      
+        public int Hours { get { return hours; }  }
+        public int Minutes { get { return minutes; } }
 
         public Time(int h, int m)
         {
-            this.minutes = 60 * h + m;
+            this.minutes =m;
             this.hours = h + m/60;
         }
         public override String ToString()
         {
-            return (minutes.ToString());
+            return (this.hours.ToString("D2")+":"+this.minutes.ToString("D2"));
         }
     }
 }
