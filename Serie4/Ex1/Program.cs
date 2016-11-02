89abc0631936610866b335402944b37a438b606b
lancelot <lancelotlancelot94@gmail.com>
2016-11-02 14:36:26 +0100

    avancement

--- Serie4/Ex1/Program.cs
+++ Serie4/Ex1/Program.cs
@@ -10,12 +10,25 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-            Time t = new Time(System.DateTime.Now.Hour,System.DateTime.Now.Minute);
-            Console.WriteLine("Temps écoulé ce jour: "+t+" minutes");
-            Console.WriteLine("Heures écoulées ce jour:(H)" + t.Hours + "==(Min)"+t.Minutes);
+            /*Time t = new Time(System.DateTime.Now.Hour, System.DateTime.Now.Minute);
+            Console.WriteLine("Temps écoulé ce jour: " + t + " minutes");
+            Console.WriteLine("Heures écoulées ce jour:(H)" + t.Hours + "==(Min)" + t.Minutes);
 
-            Console.WriteLine("test" + (float)t.Minutes/60);
-            
+            Console.WriteLine("test" + (float)t.Minutes / 60);
+            Console.WriteLine("test" + 180 % 60+" and "+180/60);
+
+            Time t2 = new Time(12, 3);
+            bool b1 = (t == t2);
+            bool b2 = (t != t2);
+            Console.WriteLine("Is t==t2? " + b1 + " and is t!=t2? " + b2);
+            */
+
+            Time t1 = new Time(8, 25);
+            Time t2 = 180; //3h
+            int m1 = (int)t1;
+            Console.WriteLine($"t1={t1} \nand t2={t2} \nand m1={m1}");
+            Time t3 = t1 + 45;
+
             Console.ReadKey();
         }
     }
@@ -23,18 +36,57 @@ namespace Ex1
     {
         private readonly int minutes;
         private readonly int hours;
-      
-        public int Hours { get { return hours; }  }
+
+        public int Hours { get { return hours; } }
         public int Minutes { get { return minutes; } }
 
         public Time(int h, int m)
         {
-            this.minutes =m;
-            this.hours = h + m/60;
+            this.minutes = m;
+            this.hours = h + m / 60;
         }
         public override String ToString()
         {
-            return (this.hours.ToString("D2")+":"+this.minutes.ToString("D2"));
+            return (this.hours.ToString("D2") + ":" + this.minutes.ToString("D2"));
+        }
+
+        //Operators overload
+        public static Boolean operator== (Time t1, Time t2)
+        {
+            if (t1.hours == t2.hours && t1.minutes == t2.minutes)
+                return true;
+            return false;
+        }
+        public static Boolean operator!= (Time t1, Time t2)
+        {
+            if (t1.hours == t2.hours && t1.minutes == t2.minutes)
+                return false;
+            return true;
+        }
+
+        //Convertion operators
+        //int to Time
+        public static implicit operator Time(int minutes)
+        {
+            int hours = 0;
+            int min = 0;
+            if (minutes > 59)
+            {
+                hours = minutes / 60;
+                min = minutes % 60;
+            }else
+            {
+                hours = 0;
+                min = minutes;
+            }
+            return new Time(hours, min);
+        }
+        public static implicit operator int(Time t)
+        {
+            int min = t.hours*60;
+            min += t.minutes;
+
+            return min;
         }
     }
 }
---
+++ Serie4/Ex2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text= File.ReadAllText("divers.txt");
+            
+
+
+
+        }
+    }
+
+}
