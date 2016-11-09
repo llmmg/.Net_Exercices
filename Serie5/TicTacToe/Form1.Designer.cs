5032fad799957dd2a7fa4b60834d955fbb09cafa
lancelot <lancelotlancelot94@gmail.com>
2016-11-09 16:58:27 +0100

    avancement

--- Serie5/TicTacToe/Form1.Designer.cs
+++ Serie5/TicTacToe/Form1.Designer.cs
@@ -29,15 +29,15 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -67,6 +67,36 @@
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(3, 313);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(225, 147);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(234, 313);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(225, 147);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(465, 313);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(225, 147);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button2_Click);
+            // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(3, 3);
@@ -85,6 +115,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -94,6 +125,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
             // 
             // button4
             // 
@@ -103,6 +135,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button2_Click);
             // 
             // button5
             // 
@@ -112,6 +145,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "button5";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button2_Click);
             // 
             // button6
             // 
@@ -121,33 +155,7 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "button6";
             this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(465, 313);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(225, 147);
-            this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(234, 313);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(225, 147);
-            this.button8.TabIndex = 7;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(3, 313);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(225, 147);
-            this.button9.TabIndex = 8;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button2_Click);
             // 
             // MainForm
             // 
--- Serie5/TicTacToe/Form1.cs
+++ Serie5/TicTacToe/Form1.cs
@@ -12,9 +12,15 @@ namespace TicTacToe
 {
     public partial class MainForm : Form
     {
+        //0 = user, 1=computer
+        int playerPlaying = 0;
+
         public MainForm()
         {
             InitializeComponent();
+
+            //Subscrib method button1_click2 to button1's click event
+            this.button1.Click += new System.EventHandler(this.button1_click2);
         }
 
         private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
@@ -28,7 +34,37 @@ namespace TicTacToe
         }
         private void button1_click2(object sender, EventArgs e)
         {
-            this.button1.Text = "LELTEST";
+            this.button1.BackColor =Color.Red;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Text = "X";
+        }
+    }
+    public class state
+    {
+        private string[,] value;
+
+        public state()
+        {
+            this.value = new string[3,3];
+        }
+
+
+        public bool win()
+        {
+            string tmp = value[0, 0];
+
+            if (tmp == value[0, 1] && tmp == value[0, 2])
+                return true;
+            
+            
+            /*for(int i=0;i<3;i++)
+            {
+                value[0,i]
+            }*/
+        }
+
     }
 }
